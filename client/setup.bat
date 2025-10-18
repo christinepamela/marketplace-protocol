affe2ad9b16c86de831e8c3c38dddeb120f272da
Pam <120184545+christinepamela@users.noreply.github.com>
2025-10-18 22:07:08 +0800

    Delete client directory

--- client/setup.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-mkdir src\components\common
-mkdir src\components\product
-mkdir src\components\identity
-mkdir src\components\payment
-mkdir src\types
-mkdir src\services
-mkdir src\utils
